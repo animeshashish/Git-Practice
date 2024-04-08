de5ab78efb8f66d23e6a064ec80ad25e6edb8483
Animesh Hemrom <animeshashishhemrom@gmail.com>
2024-04-08 08:38:14 +0530

    Value of a & b set

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -3,10 +3,10 @@ package GitPractice;
 public class AddTwoNumbers {
     public static void main(String[] args) {
 
-        final int a = 50;
-        final int b = 100;
+        int a = 50;
+        int b = 100;
         int sum = a+b;
 
-        System.out.println("final sum of two numbers a+b =" + " " + sum);
+        System.out.println("Sum of two numbers a+b =" + " " + sum);
     }
 }
