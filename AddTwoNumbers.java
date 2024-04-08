b21b27ff64597037001ca3a35da924410112f483
Animesh Hemrom <animeshashishhemrom@gmail.com>
2024-04-08 08:14:52 +0530

    Final change in value of a & b done

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -3,10 +3,10 @@ package GitPractice;
 public class AddTwoNumbers {
     public static void main(String[] args) {
 
-        int a = 10;
-        int b = 25;
+        final int a = 50;
+        final int b = 100;
         int sum = a+b;
 
-        System.out.println("Sum of two numbers a+b =" + " " + sum);
+        System.out.println("final sum of two numbers a+b =" + " " + sum);
     }
 }
