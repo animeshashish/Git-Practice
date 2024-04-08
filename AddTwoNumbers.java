a2b214c1371671da0b9312ab8dacc289ef748057
Animesh Hemrom <animeshashishhemrom@gmail.com>
2024-04-08 08:43:09 +0530

    Value of a & b changed

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -4,7 +4,9 @@ public class AddTwoNumbers {
     public static void main(String[] args) {
 
         int a = 50;
+        a = 150; // value of a is now 150
         int b = 100;
+        b = 200; // value of b is now
         int sum = a+b;
 
         System.out.println("Sum of two numbers a+b =" + " " + sum);
